7107d3a1d5c1dceb8b8c789ac1ebfa37585511cc
Cristian Marin <tian.marin@gmail.com>
2017-03-15 16:53:13 -0600

    mmm

--- wp-force-login.php
+++ wp-force-login.php
@@ -72,7 +72,7 @@ add_action('template_redirect', 'v_forcelogin');
 function my_forcelogin_whitelist( $whitelist ) {
 	$whitelist[] = network_site_url( '/calendar/' );
 	global $NOVIS_CSI_CMP_TASK;
-	$NOVIS_CSI_CMP_TASK->write_log ( $whiltelist );
+	$NOVIS_CSI_CMP_TASK->write_log ( $whitelist );
 	return $whitelist;
 }
 add_filter('v_forcelogin_whitelist', 'my_forcelogin_whitelist', 10, 1);
